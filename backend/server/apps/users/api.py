f44d34475a44195b66d7de18c14d0017cafde96d
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2026-03-11 00:32:29 +0300

    fix: use correct class name

--- backend/server/apps/users/api.py
+++ backend/server/apps/users/api.py
@@ -1,7 +1,7 @@
 from ninja import Router
 
 from .logic import functions as domain_functions
-from .logic.request_schemas import RegisterUserRequest, UpdateAuthenticatedUserSchema
+from .logic.request_schemas import RegisterUserRequest, UpdateAuthenticatedUserRequest
 from .logic.response_schemas import UserRetrieveResponse
 
 router = Router()
@@ -13,7 +13,7 @@ def get_me(request) -> UserRetrieveResponse:
 
 
 @router.patch("/me", response=UserRetrieveResponse)
-def update_me(request, data: UpdateAuthenticatedUserSchema):
+def update_me(request, data: UpdateAuthenticatedUserRequest):
     updated_user = domain_functions.update_user(data, request.auth)
     return UserRetrieveResponse.model_validate(updated_user)
 
